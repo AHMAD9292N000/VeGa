9f542665f452f38b86398c842d55934d8e981a80
AHMAD9292N000 <ahamdiwowbsk@gmail.com>
2024-06-01 04:01:39 +0200

    Update config.py

--- config.py
+++ config.py
@@ -38,7 +38,7 @@ UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/AHMAD9292N000/VeGa",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "3TB")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "vega")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
